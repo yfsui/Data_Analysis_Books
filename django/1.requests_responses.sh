f4e5661747fbb2cbdf78c45e38d7ae0b604e19b4
Yufeng Sui <anniesui9627@gmail.com>
2019-11-18 18:59:43 -0500

    add django part 1 and 2

---
+++ django/1.requests_responses.sh
@@ -0,0 +1,164 @@
+
+Django documentation
+https://docs.djangoproject.com/en/2.2/
+
+# ============= Create a poll application ============= 
+
+# It’ll consist of two parts:
+# - A public site that lets people view polls and vote in them.
+# - An admin site that lets you add, change, and delete polls.
+
+
+$ mkdir poll                        # create a new directory called "poll"
+$ cd poll/
+
+# ============= Create & Activate virtual environment =====================
+
+# Dependency problem: software packages which have dependencies on specific versions of other software packages
+# e.g. need version 2 for this project but version 3 for another
+
+# Solve dependency problem by creating different virtual environments
+# Create a new virtual environment for a new project
+# We'll have a copy of python executables in each environment
+
+$ python3 -m venv envpoll        	 # create a new virtual environment called "envpoll" for our project
+$ ls                            	 # show "envpoll"
+
+$ source envpoll/bin/activate   	 # activate virtual environment
+$ pip freeze 	                     # now show nothing
+
+$ pip install -U pip                 # upgrade pip, "U" for update
+$ pip install django                 # install django
+$ pip freeze                         # show django and other two dependencies 
+$ python -m django --version         # show the version of django installed
+
+
+
+# ============= Create a project: initial setup =====================
+
+# If this is your first time using Django, you’ll need to auto-generate some code that establishes a Django project.
+# Project is a directory of code that contains all the settings for an instance of Django, 
+# including database configuration, Django-specific options and application-specific settings.
+
+# "startproject" creates a Django project directory structure for the given project name in the current directory or the given destination.
+# By default, the new directory contains manage.py and a project package (containing a settings.py and other files).
+
+$ django-admin startproject mysite          # create a "mysite" directory in your current directory
+$ ls                                        # show envpoll and mysite
+
+$ cd mysite/
+$ ls                                        # show manage.py, mysite, db.sqlite3
+
+# inner "mysite/" directory is the actual Python package for your project
+$ less mysite/ + press "tab" twice          # show -> __init__.py   settings.py   urls.py   wsgi.py
+# __init__.py: an empty file that tells Python that this directory should be considered a Python package
+# settings.py: settings/configuration for this Django project
+# urls.py:     URL declarations for this Django project; a “table of contents” of your Django-powered site
+# wsgi.py:     an entry-point for WSGI-compatible web servers to serve your project
+
+
+
+# ============= Start the Django development server =====================
+
+# A lightweight Web server written purely in Python
+
+# change into the outer mysite directory
+$ cd ..              
+$ cd mysite/       
+$ ls                                        # show manage.py and mysite
+$ sudo /home/ys3273/poll/envpoll/bin/python manage.py runserver 0.0.0.0:80
+# Go back to external IP -> show "DisallowedHost"
+
+$ pwd                                       # return /home/ys3273/poll/mysite/mysite
+$ vim settings.py                           # change allowed host to ['*']
+# Go back to external IP -> show "Congratulations!" with a little rocket :)
+# Now that your environment – a “project” – is set up
+
+
+# ============= Create the Polls app  =====================
+
+# Each application you write in Django consists of a Python package that follows a certain convention. 
+# Django comes with a utility that automatically generates the basic directory structure of an app, 
+# so you can focus on writing code rather than creating directories.
+
+# Difference between a project and an app? 
+# An app is a Web application that does something (e.g., a Weblog system, a database of public records or a simple poll app)
+# A project is a collection of configuration and apps for a particular website. 
+# A project can contain multiple apps. 
+# An app can be in multiple projects.
+
+# To create your app, make sure you’re in the same directory as manage.py
+# so that it can be imported as its own top-level module, rather than a submodule of mysite.
+
+$ python manage.py startapp polls 
+$ ls    									 # show -> db.sqlite3  manage.py  mysite  polls
+
+# "polls" directory structure will house the poll application.
+$ cd polls/
+$ ls  
+
+
+# ============= Write your first view =====================
+
+$ vim views.py               # import and def "index" view
+
+		from django.http import HttpResponse
+
+		def index(request):
+		    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+# ============= Map view to a URL =====================
+
+# To call the view, we need to map it to a URL - and for this we need a URLconf.
+# To create a URLconf in the polls directory, create a file called urls.py
+$ vim urls.py
+
+		from django.urls import path
+		from . import views
+
+		urlpatterns = [
+		    path('', views.index, name='index'),
+		]
+
+# path() function is passed four arguments
+# - two required: route, view
+# - two optional: kwargs, name
+
+# route: a string that contains a URL pattern. 
+# When processing a request, Django starts at the first pattern in urlpatterns and makes its way down the list, 
+# comparing the requested URL against each pattern until it finds one that matches.
+
+# view: When Django finds a matching pattern,
+# it calls the specified view function with an HttpRequest object as the first argument and any “captured” values from the route as keyword arguments.
+# "views.index" sends back this http response
+
+
+# The next step is to point the root URLconf at the polls.urls module.
+$ cd ..          			# back to ~/poll/mysite
+$ cd mysite/     			# theng go to ~/poll/mysite/mysite
+$ vim urls.py
+
+		from django.contrib import admin
+		from django.urls import include, path
+
+		urlpatterns = [
+		    path('polls/', include('polls.urls')),
+		    path('admin/', admin.site.urls),
+		]
+
+# include() function allows referencing other URLconfs
+# Whenever Django encounters include(), it chops off whatever part of the URL matched up to that point 
+# and sends the remaining string to the included URLconf for further processing.
+
+# include() makes it easy to plug-and-play URLs. 
+# Since polls are in their own URLconf (polls/urls.py), they can be placed under “/polls/” or “/content/polls/”, 
+# or any other path root, and the app will still work.
+# Always use include() when you include other URL patterns
+
+
+$ cd ..          			# back to ~/poll/mysite
+$ sudo /home/ys3273/project/env/bin/python manage.py runserver 0.0.0.0:80
+# Go back to external IP: change address to 35.245.82.219/polls/ 
+# -> show the text defined in the "index" view: "Hello, world. You're at the polls index."
+
---
+++ django/2.models_admin_site.sh
@@ -0,0 +1,293 @@
+# ================ Database setup ================
+
+$ cd poll/mysite/              # go to ~/poll/mysite
+$ vim mysite/settings.py
+
+
+# DATABASES dict:
+# 'ENGINE': By default, the configuration uses SQLite. If you’re just interested in trying Django, this is the easiest choice. 
+# SQLite is included in Python, so you won’t need to install anything else to support your database. 
+# When starting your real project, you may want to use a more scalable database like PostgreSQL.
+
+# 'NAME': the name of your database
+# If you’re using SQLite, the database will be a file on your computer: full absolute path, including filename
+# Default value: os.path.join(BASE_DIR, 'db.sqlite3') will store the file in your project directory.
+
+
+# Change time zone
+		TIME_ZONE = 'America/New_York'
+
+# INSTALLED_APPS: holds the names of all Django applications that are activated in this Django instance. 
+# Apps can be used in multiple projects, and you can package and distribute them for use by others in their projects.
+# Common applications are included by default.
+
+
+
+# Some of these applications make use of at least one database table, 
+# so we need to create the tables in the database before we can use them.
+
+$ python manage.py migrate
+# The migrate command looks at apps in the INSTALLED_APPS setting, 
+# and creates any necessary database tables according to the database settings in your mysite/settings.py file and the database migrations shipped with the app
+
+
+
+
+# ================ Create models ================
+
+# Model: contains database layout, essential fields and behaviors of the data
+
+# Three steps to make model changes:
+# 1. Change your models (in models.py)
+# 2. Create migrations for those changes: python manage.py makemigrations
+# 3. Apply those changes to the database: python manage.py migrate
+
+
+# === 1. Change your models (in models.py) ===
+
+$ pwd                        # /home/ys3273/poll/mysite/polls
+$ vim models.py 
+
+# We’ll create two models: Question and Choice. 
+# - A Question has a question and a publication date. 
+# - A Choice has two fields: the text of the choice and a vote tally 记录
+# Each Choice is associated with a Question.
+
+		from django.db import models
+
+		class Question(models.Model):
+		    question_text = models.CharField(max_length=200)
+		    pub_date = models.DateTimeField('date published')
+
+		class Choice(models.Model):
+		    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+		    choice_text = models.CharField(max_length=200)
+		    votes = models.IntegerField(default=0)
+
+# Each model is represented by a class that subclasses django.db.models.Model. 
+# Each model has a number of class variables, each of which represents a database field in the model.
+
+# Column names (machine-readable): question_text, pub_date, ...
+# You can use an optional first positional argument to a Field to designate a human-readable name (e.g. 'date published')
+
+# Some Field classes have required arguments. 
+# e.g. CharField requires that you give it a max_length.
+
+# ForeignKey: define table relationship
+
+
+
+# To include the app in our project, we need to add a reference to its configuration class in the INSTALLED_APPS setting.
+$ vim settings.py            # pwd: /home/ys3273/poll/mysite/mysite
+
+		INSTALLED_APPS = [
+		    'polls.apps.PollsConfig',
+		    'django.contrib.admin',
+		    'django.contrib.auth',
+		    'django.contrib.contenttypes',
+		    'django.contrib.sessions',
+		    'django.contrib.messages',
+		    'django.contrib.staticfiles',
+		]
+
+
+# === 2.Create migrations for those changes ===
+
+# Migrations are how Django stores changes to your models (and thus your database schema) 
+$ cd ..                                    # ~/poll/mysite
+$ python manage.py makemigrations polls
+
+# Returns:
+		# Migrations for 'polls':
+		#   polls/migrations/0001_initial.py
+		#     - Create model Question
+		#     - Create model Choice
+
+$ python manage.py sqlmigrate polls 0001     # takes migration names and returns what SQL that migration would run (vary depending on the database)
+
+
+# === 3. Apply changes to the database ===
+
+# The migrate command takes all the migrations that haven’t been applied and runs them against your database
+# synchronizing the changes you made to your models with the schema in the database.
+$ python manage.py migrate 
+
+
+
+
+# ================ Explore the database API ================
+
+$ python manage.py shell    					    # invoke interactive Python shell 
+
+In: from polls.models import Choice, Question       # import the model classes we just wrote
+
+In: Question.objects.all() 							# objects.all() displays all the questions in the database
+# Out: <QuerySet []>								# no questions are in the system yet
+
+# Create a new Question.
+# Support for time zones is enabled in the default settings file, so
+# Django expects a datetime with tzinfo for pub_date. 
+# Use timezone.now() instead of datetime.datetime.now() and it will do the right thing.
+In: from django.utils import timezone
+In: q = Question(question_text="What's new?", pub_date=timezone.now())
+In: q.save()     # Save the object into the database
+
+In: q.id   		 
+# Out: 1
+
+In: q.question_text 	
+# Out: "What's new?"
+
+In: q.pub_date          
+# Out: datetime.datetime(2019, 11, 17, 20, 1, 17, 692704, tzinfo=<UTC>)
+
+In: q.question_text = "What's up?"     # change values and save                                                  
+In: q.save()
+
+In: Question.objects.all() 			   
+# Out: <QuerySet [<Question: Question object (1)>]>        # not useful, def __str__ to fix it
+
+In: exit()
+
+
+
+# === Define representation of Question object ===
+
+$ cd polls/
+$ vim models.py
+
+		import datetime
+		from django.db import models
+		from django.utils import timezone
+
+		class Question(models.Model):
+		    # ...
+		    def __str__(self):
+		        return self.question_text
+
+		    def was_published_recently(self):
+        		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+		class Choice(models.Model):
+		    # ...
+		    def __str__(self):
+		        return self.choice_text
+
+
+# Go back to shell to test
+$ cd ..
+$ python manage.py shell 
+
+
+In: from polls.models import Choice, Question       
+
+In: Question.objects.all() 	
+# Out: <QuerySet [<Question: What's up?>]> 
+
+
+
+# === Get data, filter ===
+
+In: Question.objects.filter(id=1)                                                         
+# Out: <QuerySet [<Question: What's up?>]>
+
+In: Question.objects.filter(question_text__startswith='What')                             
+# Out: <QuerySet [<Question: What's up?>]>
+
+In: from django.utils import timezone
+In: current_year = timezone.now().year
+In: Question.objects.get(pub_date__year=current_year)   # Get the question that was published this year.
+# Out: <Question: What's up?>
+
+In: Question.objects.get(pk=1)  # primary-key exact lookups, equivalent to Question.objects.get(id=1)
+# Out: <Question: What's up?>
+
+In: q = Question.objects.get(pk=1)
+In: q.was_published_recently()
+# Out: True
+
+
+
+
+# === Give the Question a couple of Choices ===
+
+In: q.choice_set.all()
+# Out: <QuerySet []>
+
+# Create three choices.
+In: q.choice_set.create(choice_text='Not much', votes=0)
+# Out: <Choice: Not much>
+In: q.choice_set.create(choice_text='The sky', votes=0)
+# Out: <Choice: The sky>
+In: c = q.choice_set.create(choice_text='Just hacking again', votes=0)
+
+# Question objects get access to Choice objects.
+In: q.choice_set.all()                                                                   
+# Out: <QuerySet [<Choice: Not much>, <Choice: The sky>, <Choice: Just hacking again>]>
+
+In: q.choice_set.count()                                                                 
+# Out: 3
+
+# Choice objects also have API access to their related Question objects.
+In: c.question                                                                            
+# Out: <Question: What's up?>
+
+
+# Use double underscores to separate relationships. This works as many levels deep as you want; there's no limit.
+# Find all Choices for any question whose pub_date is in this year
+In: Choice.objects.filter(question__pub_date__year=current_year)
+# Out: <QuerySet [<Choice: Not much>, <Choice: The sky>, <Choice: Just hacking again>]>
+
+# Delete one of the choices
+In: c = q.choice_set.filter(choice_text__startswith='Just hacking')
+In: c.delete()
+
+
+# ==================== Django Admin =====================
+
+# Generating admin sites for your staff or clients to add, change, and delete content is tedious work that doesn’t require much creativity. 
+# For that reason, Django entirely automates creation of admin interfaces for models.
+
+# Create an admin user who can login to the admin site
+$ python manage.py createsuperuser
+Username (leave blank to use 'ys3273'): admin
+Email address: anniesui9627@gmail.com
+Password: polls9627
+
+
+# Start the development server 
+$ sudo /home/ys3273/project/env/bin/python manage.py runserver 0.0.0.0:80
+# Go back to external IP: change address to 35.245.82.219/admin/
+# Login using admin and polls9627
+# See Django admin index page. "groups" and "users" are provided by django.contrib.auth, the authentication framework shipped by Django.
+
+
+
+# ==================== Make the polls app modifiable in the admin =====================
+
+# Tell the admin that Question objects have an admin interface: register "Question" model in admin
+
+$ vim polls/admin.py
+
+		from django.contrib import admin
+		from .models import Question
+
+		admin.site.register(Question)
+
+# Go back to external IP: polls app displayed
+# Click “Questions”
+# Now you’re at the “change list” page for questions. This page displays all the questions in the database and lets you choose one to change it. 
+
+# Click the “What’s up?” question to edit it:
+# * Save – Saves changes and returns to the change-list page for this type of object.
+# * Save and continue editing – Saves changes and reloads the admin page for this object.
+# * Save and add another – Saves changes and loads a new, blank form for this type of object.
+# * Delete – Displays a delete confirmation page.
+
+# Change the “Date published” by clicking the “Today” and “Now” shortcuts. 
+# Click “Save and continue editing.”
+# Click “History”: see all changes made to this object via the Django admin
+
+
+
+
